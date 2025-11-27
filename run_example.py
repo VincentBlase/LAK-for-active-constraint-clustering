31991cc475e9296f3fcc1c298de5a7e193de7370
VincentBlase <63648607+VincentBlase@users.noreply.github.com>
2025-11-27 10:18:46 +0100

    Update run_example.py

--- run_example.py
+++ run_example.py
@@ -3,7 +3,7 @@ from LAK.clustering import clustering
 from mmd_critic.kernels import RBFKernel
 from LAK.open_data import open_dataset
 from LAK.compute_score import compute_score
-from LAK.LAK import Locally_adaptive_kernel
+from LAK.LAK import adaptive_rbf_kernel
 
 name='IRIS'
 X,y = open_dataset(name)
@@ -15,7 +15,7 @@ ARI_FALCON = []
 NMI_FALCON = []
 silhouette_FALCON = []
 
-clusterer_FALCON = FALCON(X,y,Locally_adaptive_kernel, Locally_adaptive_kernel)
+clusterer_FALCON = FALCON(X,y,adaptive_rbf_kernel, adaptive_rbf_kernel)
 clustering_FALCON, intermediate_clustering = clusterer_FALCON.cluster(n=max_budget,k_proto=2, train_indices = None)
     
 ARI_FALCON, NMI_FALCON, silhouette_FALCON = compute_score(intermediate_clustering,max_budget)
